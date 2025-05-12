aa047911267cf558ec5eab705eeb41fba89763c7
Miguel-Trevizan <160758221+Miguel-Trevizan@users.noreply.github.com>
2025-05-12 12:36:41 -0300

    feww

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+  'https://rkqupiqbtngcyrdunuxc.supabase.co',
   'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrcXVwaXFidG5nY3lyZHVudXhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU2NTYsImV4cCI6MjA2MjExMTY1Nn0.FTIdlT2yLGvGnZUpx22jYnvRPLOE8n3MGZLddo27mNI'
 )
